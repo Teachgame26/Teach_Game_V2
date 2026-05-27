b25f1dcb477cdbefc9c6a896514a1297b2299baa
ined2011 <mayerlindained@gmail.com>
2026-05-26 19:18:23 -0500

    Configurar base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,21 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Teach_Game_V2/',
+  // eslint-disable-next-line no-undef
+  base: process.env.VITE_BASE_PATH || '/',
   plugins: [react()],
 })
+
+
+
+
+//import { defineConfig } from 'vite'
+//import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+//export default defineConfig({
+  //base: '/Teach_Game_V2/',
+  //plugins: [react()],
+//})
+
+
